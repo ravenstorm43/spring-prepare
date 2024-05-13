89698ec78219707c290d759f6301d2193dd81cc5
ravenstorm43 <201912065@office.induk.ac.kr>
2024-05-13 12:24:41 +0900

    첫 api 구현

--- src/main/java/com/sparta/springprepare/SpringPrepareApplication.java
+++ src/main/java/com/sparta/springprepare/SpringPrepareApplication.java
@@ -1,4 +1,4 @@
-package com.example.springprepare;
+package com.sparta.springprepare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
